d831cdc10b2234f4c2e708a9502e846da943a8dd
Swapan <swapan@yahoo.com>
2013-01-16 08:53:32 -0600

    Added more code to main driver

--- alarmDriver.c
+++ alarmDriver.c
@@ -26,6 +26,48 @@
 static volatile char dtmf_val = 0 ;
 static volatile char code[16] ;
 
+int genTeltone (int tone)
+{
+  if (tone == DIALTONE)
+    softPwmCreate (TONE_PIN, DTONE_VALUE, DTONE_RANGE) ;
+  else if (tone == KISSTONE)
+    softPwmCreate (TONE_PIN, KTONE_VALUE, KTONE_RANGE) ;
+  else {
+    fprintf (stdout, "Wrong tone code - %i\n", tone) ;
+    return 1 ;
+  }
+  return 0;
+}
+
+static PI_THREAD (dtmfHandler) {
+  (void)piHiPri (10);	// Set this thread to be high priority
+  for (;;) {
+    genTeltone (DIALTONE);	// phone can go off-hook only if there is dial-tone
+
+    while (digitalRead(RINGPIN) == HIGH) {	// phone is off-hook
+      // at this time panel calls central stn number and once it is off-hook
+      delay (1000);				// and waits for 1 sec
+      // at this time panel sends Contact-ID handshake which is
+      // 100mSec of 1400Hz pure-tone + 100mSec of silence + 100mSec of 2300Hz pure-tone
+      // 250mSec after that it sends Contact-ID message of 16 DTMF digits
+      delay (550);
+      // each digit is 50mSec long separated by 50mSec silence
+      // last digit is checksum digit
+      i = 16;
+      while (i > 0) {
+        code[16 - i] = dtmf_val;
+        delay (50);
+        i -= 1;
+      }
+      // after the central stn succesfully receives the msg it sends a kiss-off msg
+      // which is 800mSec of 1400Hz pure-tone
+      genTeltone (KISSTONE);
+      delay (800);
+      // if panel does not receive the kiss-off tone then it retransmits the msg
+    }
+  }
+}
+
 /*
  * intrDTMF: routine to decode DTMF tone values
  *********************************************************************************
@@ -45,15 +87,8 @@ void intrDTMF (void)
     case 0:
       dtmf_val = 'D';
       break;
-    case 1:
-    case 2:
-    case 3:
-    case 4:
-    case 5:
-    case 6:
-    case 7:
-    case 8:
-    case 9:
+    case 1: case 2: case 3: case 4: case 5:
+    case 6: case 7: case 8: case 9:
       dtmf_val = reg_val + 48;
       break;
     case 10:
@@ -80,27 +115,11 @@ void intrDTMF (void)
   }
 }
 
-int genTeltone (int tone)
-{
-  if (tone == DIALTONE)
-    softPwmCreate (TONE_PIN, DTONE_VALUE, DTONE_RANGE) ;
-  else if (tone == KISSTONE)
-    softPwmCreate (TONE_PIN, KTONE_VALUE, KTONE_RANGE) ;
-  else {
-    fprintf (stdout, "Wrong tone code - %i\n", tone) ;
-    return 1 ;
-  }
-  return 0;
-}
-
-int main ()
-{
-  int i;
-
+int setup (void) {
   if (wiringPiSetup () == -1)
   {
-    fprintf (stdout, "oops: %s\n", strerror (errno)) ;
-    return 1 ;
+    fprintf (stdout, "oops: %s\n", strerror (errno));  fflush (stderr);
+    return -1;
   }
 
   pinMode(Q1PIN, INPUT);
@@ -111,37 +130,111 @@ int main ()
   pinMode(RINGPIN, INPUT);
 
   if (wiringPiISR (LATCHPIN, INT_EDGE_RISING, &intrDTMF) < 0) {
-    fprintf (stderr, "Unable to setup ISR: %s\n", strerror(errno)) ;
-    return 1 ;
+    fprintf (stderr, "Unable to setup ISR: %s\n", strerror(errno));  fflush (stderr);
+    return -1;
   }
-
-  printf ("Starting the main alarm routine ... ") ; fflush (stdout) ;
-
-  for (;;) {
-    genTeltone (DIALTONE);	// phone can go off-hook only if there is dial-tone
-
-    while (digitalRead(RINGPIN) == HIGH) {	// phone is off-hook
-      // at this time panel calls central stn number and once it is off-hook
-      delay (1000);				// and waits for 1 sec
-      // at this time panel sends Contact-ID handshake which is
-      // 100mSec of 1400Hz pure-tone + 100mSec of silence + 100mSec of 2300Hz pure-tone
-      // 250mSec after that it sends Contact-ID message of 16 DTMF digits
-      delay (550);
-      // each digit is 50mSec long separated by 50mSec silence
-      // last digit is checksum digit
-      i = 16;
-      while (i > 0) {
-        code[16 - i] = dtmf_val;
-        delay (50);
-        i -= 1;
-      }
-      // after the central stn succesfully receives the msg it sends a kiss-off msg
-      // which is 800mSec of 1400Hz pure-tone
-      genTeltone (KISSTONE);
-      delay (800);
-      // if panel does not receive the kiss-off tone then it retransmits the msg
-    }
+  if (piThreadCreate (dtmfHandler) < 0) {
+    fprintf (stderr, "Unable to create dtmfHandler thread: %s\n", strerror(errno));  fflush (stderr);
+    return -1;
   }
+}
+
+int main () {
+  int serverfd, i;
+  struct sockaddr_in serv_addr;
+
+  if (setup() < 0) return 1;
+
+  printf ("Starting the main alarm routine ... "); fflush (stdout) ;
+  serverfd = socket(AF_INET, SOCK_STREAM, 0) < 0);
+  memset(&serv_addr, '0', sizeof(serv_addr));
+
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = INADDR_ANY;
+  serv_addr.sin_port = htons(2001);
+
+  if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) { 
+    fprintf (stderr, "ERROR on binding.\n");  fflush (stderr);
+    return 1;
+  }
+  listen(serverfd, 5);	// at most 5 connection
+  handle_server(serverfd);
 
   return 0;
+}
+
+int handle_server (serverfd) {
+  int connfd, alarm_status = 0, debug_lvl = 0;
+  char buffer[256], *input, output[1024];
+
+  memset(buffer, '0', sizeof(buffer));
+  memset(output, '0', sizeof(output));
+  while(1) {
+    connfd = accept(serverfd, (struct sockaddr*)NULL, NULL); 
+    if (read(connfd, buffer, 255) < 0) {
+      fprintf (stderr, "ERROR reading from socket.\n");
+    } 
+    input = trim( buffer );
+    command = input[0];
+    switch (command) {
+      case '?':
+        sprintf(output, "? - this help message\nS - print current system status\nL - print event log\nN - program an email address\nP - set/change password\nD - set system debug level\nC - close this session\nA - generate and artificial Contact-ID msg\n");
+        write(connfd, output, strlen(output));
+        break;
+      case 'C':
+      case 'c':
+        close(connfd);
+        break;
+      case 'S':
+      case 's':
+        if (alarm_status == 0) sprintf(output, "Alarm interface is active and working.\n");
+        else sprintf(output, "Alarm interface is in error.\n");
+        write(connfd, output, strlen(output));
+        break;
+      case 'L':
+      case 'l':
+        break;
+      case 'N':
+      case 'n':
+        input++; trim( input );
+        email_addr = input;
+        break;
+      case 'P':
+      case 'p':
+        break;
+      case 'D':
+      case 'd':
+        input++; trim( input );
+        debug_lvl = atoi( input );
+        if (debug_lvl > 5) debug_lvl = 5;
+        if (debug_lvl < 0) debug_lvl = 0;
+        break;
+      case 'A':
+      case 'a':
+        sprintf(output, "000118122010065");
+        write(connfd, output, strlen(output));
+        break;
+    } 
+    sleep(1);
+  }
+}
+
+char *trim(char *str)
+{
+  char *end;
+
+  // Trim leading space
+  while(isspace(*str)) str++;
+
+  if(*str == 0)  // All spaces?
+    return str;
+
+  // Trim trailing space
+  end = str + strlen(str) - 1;
+  while(end > str && isspace(*end)) end--;
+
+  // Write new null terminator
+  *(end+1) = 0;
+
+  return str;
 }
